b82bd4b451cab23cdc38f5a60b45f4fe51ade87b
robbyoc <81818027+robbyoc@users.noreply.github.com>
2023-06-02 13:44:17 +0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,9 +2,8 @@ const Hapi = require('@hapi/hapi');
 const routes = require('./routes.js');
 
 const init = async () => {
-  const port = process.env.PORT || 5000;
   const server = Hapi.server({
-    port,
+    port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
         cors: {
